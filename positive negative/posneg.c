a2feba930cf32b336fbbfde207d1b99e69b2c1d2
pranitadharmik <dharmikpanita@gmail.com>
2024-09-20 23:51:37 +0530

    positive negative number

---
+++ positive negative/posneg.c
@@ -0,0 +1,16 @@
+void main()
+{
+    int number;
+    printf("enter any number=");
+    scanf("%d",&number);
+    
+    if(number>0)
+    {
+        printf("entered number is POSITIVE");
+    }
+    else
+    {
+        printf("entered number is NEGATIVE");
+    }
+
+}
